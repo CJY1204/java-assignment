534252a118e448f105a328d1637cb7107584f23d
CJY1204 <junyuc1204@gmail.com>
2026-04-18 20:38:21 +0800

    Refactor SilverMembership class with sauna access feature

--- FitnessClubSystem/SilverMembership.java
+++ FitnessClubSystem/SilverMembership.java
@@ -1,9 +1,34 @@
 package FitnessClubSystem;
 
+/*
+ * SilverMembership Class
+ * ------------------------
+ * Represents the Silver tier membership.
+ * Unique attribute: hasSaunaAccess
+ * 
+ * @author Cheng Jun Yu
+ * @version 2.0
+ */
 public class SilverMembership extends Membership {
 
-    public SilverMembership() {
-        super("Silver", 100.0);
+    private static final double DEFAULT_PRICE = 100.0;
+    private boolean hasSaunaAccess;
+
+    /*
+     * Parameterized constructor for SilverMembership
+     * @param hasSaunaAccess Whether the member has sauna access
+     */
+    public SilverMembership(boolean hasSaunaAccess) {
+        super("Silver", DEFAULT_PRICE);
+        this.hasSaunaAccess = hasSaunaAccess;
+    }
+
+    /*
+     * Sets the sauna access privilege
+     * @param hasSaunaAccess New sauna access value
+     */
+    public void setHasSaunaAccess(boolean hasSaunaAccess) {
+        this.hasSaunaAccess = hasSaunaAccess;
     }
 
     @Override
@@ -11,15 +36,20 @@ public class SilverMembership extends Membership {
         return price;
     }
 
-
     @Override
     public boolean hasPriorityBooking() {
         return false;
     }
 
-
     @Override
     public boolean hasSaunaAccess() {
-        return true;
+        return hasSaunaAccess;
     }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" | Sauna Access: %s", 
+            hasSaunaAccess ? "Yes" : "No");
+    }
+
 }
