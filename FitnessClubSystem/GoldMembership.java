b185c32a59eba58a00646c8e8fa6c66bc2449270
CJY1204 <junyuc1204@gmail.com>
2026-04-18 20:37:02 +0800

    Enhance GoldMembership with new features
    Added attributes for priority booking and sauna access, and updated methods accordingly.

--- FitnessClubSystem/GoldMembership.java
+++ FitnessClubSystem/GoldMembership.java
@@ -1,9 +1,45 @@
 package FitnessClubSystem;
 
+/*
+ * GoldMembership Class
+ * ------------------------
+ * Represents the Gold tier membership (highest tier).
+ * Unique attributes: hasPriorityBooking, hasSaunaAccess
+ * 
+ * @author Cheng Jun Yu
+ * @version 2.0
+ */
 public class GoldMembership extends Membership {
 
-    public GoldMembership() {
-        super("Gold", 200.0);
+    private static final double DEFAULT_PRICE = 200.0;
+    private boolean hasPriorityBooking;
+    private boolean hasSaunaAccess;
+
+    /*
+     * Parameterized constructor for GoldMembership
+     * @param hasPriorityBooking Whether the member has priority booking privilege
+     * @param hasSaunaAccess Whether the member has sauna access
+     */
+    public GoldMembership(boolean hasPriorityBooking, boolean hasSaunaAccess) {
+        super("Gold", DEFAULT_PRICE);
+        this.hasPriorityBooking = hasPriorityBooking;
+        this.hasSaunaAccess = hasSaunaAccess;
+    }
+
+    /*
+     * Sets the priority booking privilege
+     * @param hasPriorityBooking New priority booking value
+     */
+    public void setHasPriorityBooking(boolean hasPriorityBooking) {
+        this.hasPriorityBooking = hasPriorityBooking;
+    }
+
+    /*
+     * Sets the sauna access privilege
+     * @param hasSaunaAccess New sauna access value
+     */
+    public void setHasSaunaAccess(boolean hasSaunaAccess) {
+        this.hasSaunaAccess = hasSaunaAccess;
     }
 
     @Override
@@ -11,16 +47,21 @@ public class GoldMembership extends Membership {
         return price;
     }
 
-
     @Override
     public boolean hasPriorityBooking() {
-        return true;
+        return hasPriorityBooking;
     }
 
     @Override
     public boolean hasSaunaAccess() {
-        return true;
+        return hasSaunaAccess;
     }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" | Priority Booking: %s | Sauna Access: %s",
+            hasPriorityBooking ? "Yes" : "No",
+            hasSaunaAccess ? "Yes" : "No");
+    }
+
 }
-
-
