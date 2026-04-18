a3e60a8818b9d2a1d38e9eb90e27dc92a9a869e5
CJY1204 <junyuc1204@gmail.com>
2026-04-18 20:22:59 +0800

    Update BasicMembership.java

--- FitnessClubSystem/BasicMembership.java
+++ FitnessClubSystem/BasicMembership.java
@@ -1,9 +1,42 @@
 package FitnessClubSystem;
 
+/*
+ * BasicMembership Class
+ * ------------------------
+ * Represents the Basic tier membership.
+ * Unique attribute: maxMonthlyBookings
+ * 
+ * @author Cheng Jun Yu
+ * @version 2.0
+ */
 public class BasicMembership extends Membership {
 
-    public BasicMembership() {
-        super("Basic", 50.0);
+    private static final double DEFAULT_PRICE = 50.0;
+    private int maxMonthlyBookings;
+
+    /*
+     * Parameterized constructor for BasicMembership
+     * @param maxMonthlyBookings Maximum number of class bookings allowed per month
+     */
+    public BasicMembership(int maxMonthlyBookings) {
+        super("Basic", DEFAULT_PRICE);
+        this.maxMonthlyBookings = maxMonthlyBookings;
+    }
+
+    /*
+     * Gets the maximum monthly bookings allowed
+     * @return maxMonthlyBookings
+     */
+    public int getMaxMonthlyBookings() {
+        return maxMonthlyBookings;
+    }
+
+    /*
+     * Sets the maximum monthly bookings allowed
+     * @param maxMonthlyBookings New maximum value
+     */
+    public void setMaxMonthlyBookings(int maxMonthlyBookings) {
+        this.maxMonthlyBookings = maxMonthlyBookings;
     }
 
     @Override
@@ -20,4 +53,10 @@ public class BasicMembership extends Membership {
     public boolean hasSaunaAccess() {
         return false;
     }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" | Max Bookings/Month: %d", maxMonthlyBookings);
+    }
+
 }
